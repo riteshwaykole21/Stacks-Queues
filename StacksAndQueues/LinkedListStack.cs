2318a24ce010c22f953f3168b8bd06369862a8e4
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-01-11 21:43:31 +0530

    [Ritesh]add : creting costom stack

---
+++ StacksAndQueues/LinkedListStack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    public class LinkedListStack
+    {
+        public Node top;
+        public void Push(int data)   //UC1
+        {
+            Node newNode = new Node(data);
+                if (top != null)
+                {
+                   newNode.next = top;
+                }
+            top = newNode;
+            Console.WriteLine($"New Node {data} is added");
+        }
+        public void Display()
+        {
+            Console.WriteLine("Displaying Nodes");
+            Node temp = this.top;
+            if(temp == null)
+            {
+                Console.WriteLine("List is Empty");
+                return;
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.WriteLine("Values in the Stack :"+temp.data);
+                    temp = temp.next;
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ StacksAndQueues/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Stack Program in data Structure");
+            Console.WriteLine("Please Select as per the Below Given Option : \n" +
+               "Press 1: Creating coutom stack using LinkedList.\n");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    LinkedListStack stackObject = new LinkedListStack();
+                    stackObject.Push(70);
+                    stackObject.Push(30);
+                    stackObject.Push(56);
+                    stackObject.Display();
+                    break;
+                default:
+                    break;
+            }
+            Console.ReadLine();
+              
+        }
+    }
+}
